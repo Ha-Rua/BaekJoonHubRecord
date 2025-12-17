b795801bbd228ee5764e2bda598825956161e444
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2025-12-18 00:08:23 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2556 ms, Memory: 3396 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> arr[10001];
+bool vis[10001];
+int cnt[10001];
+int res = 0;
+void DFS(int n);
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        arr[b].push_back(a);
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        DFS(i);
+        cnt[i] = res;
+        res = 0;
+        fill(vis, vis + 10001, false);
+    }
+    int maxi = *max_element(cnt + 1, cnt + n + 1);
+    for(int i = 1; i <= n; i++)
+        if(cnt[i] == maxi) cout << i << " ";
+    return 0;
+}
+void DFS(int n)
+{
+    if (vis[n]) return; 
+    vis[n] = true;
+    res++;
+    for(int i = 0; i < arr[n].size(); i++)
+    {
+        int next = arr[n][i];
+        if (!vis[next]) { 
+            DFS(next);
+        }
+    }
+}
