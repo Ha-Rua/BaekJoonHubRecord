815a6510205f9f41f7850c7a51231ff120e16825
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2025-12-19 00:29:20 +0900

    [Gold IV] Title: 이분 그래프, Time: 192 ms, Memory: 11348 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> arr[20001];
+int vis[20001];
+bool isBipartite = true;
+void DFS(int n);
+int main()
+{
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    int k, n, m;
+    cin >> k;
+    while (k--)
+    {
+        cin >> n >> m;
+        for (int i = 1; i <= n; i++) {
+            arr[i].clear();
+            vis[i] = 0;
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            arr[a].push_back(b);
+            arr[b].push_back(a);
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (!vis[i])
+            {
+                vis[i] = 1;
+                DFS(i);
+            }
+        }
+        cout << (isBipartite ? "YES\n" : "NO\n");
+        isBipartite = true;
+    }
+    return 0;
+}
+void DFS(int n)
+{
+    for(int next : arr[n])
+    {
+        if (!vis[next])
+        {
+            vis[next] = 3 - vis[n];
+            DFS(next);
+        }
+        else if (vis[next] == vis[n])
+        {
+            isBipartite = false;
+            return;
+        }
+    }
+}
