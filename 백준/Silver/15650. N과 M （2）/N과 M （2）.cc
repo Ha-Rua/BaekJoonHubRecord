b7fedc41dcfb480dacedac20a232f2f0dd589c43
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2025-12-28 01:17:25 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, m;
+int arr[71];
+void dfs(int cnt, int start)
+{
+    if (cnt == m + 1)
+    {
+        for (int i = 1; i <= m; i++) cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = start; i <= n; i++) {
+        arr[cnt] = i;
+        dfs(cnt + 1, i + 1);
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    dfs(1, 1);
+    return 0;
+}
