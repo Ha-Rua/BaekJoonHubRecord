0202ce7af4d0569a82a08b5d9d472c2c89ba67bf
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2025-12-28 23:55:07 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 104 ms, Memory: 8024 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+vector<int> graph[100001];
+int parent[100001];
+bool visited[100001];
+
+void BFS(int v)
+{
+    queue<int> q;
+    q.push(v);
+    visited[v] = true;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int i = 0; i < graph[u].size(); i++) {
+            if (!visited[graph[u][i]]) {
+                q.push(graph[u][i]);
+                visited[graph[u][i]] = true;
+                parent[graph[u][i]] = u;
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int u, v; cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    BFS(1);
+    for (int i = 2; i <= n; i++)
+        cout << parent[i] << '\n';
+}
