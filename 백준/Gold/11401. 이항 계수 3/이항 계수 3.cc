5a972cf0042380d696a4b620bb9ca3b37bbbf77e
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2026-01-03 23:48:30 +0900

    [Gold I] Title: 이항 계수 3, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+ll m = 1000000007;
+long long func1(int n, int k)
+{
+    long long res = 1;
+    for (int i = 0; i < k; i++)
+        res = (res * (n - i)) % m;
+    return res % m;
+}
+long long func2(ll x, ll y, ll z)
+{
+    if (y == 1) return x % z;
+    ll val = func2(x, y / 2, z);
+    val = val * val % z;
+    if (y % 2 == 1) val = val * x % z;
+    return val % z;
+}
+int main()
+{
+    int n, k; cin >> n >> k;
+    cout << (func1(n, k) * func2(func1(k, k), m - 2, m)) % m;
+    return 0;
+}
