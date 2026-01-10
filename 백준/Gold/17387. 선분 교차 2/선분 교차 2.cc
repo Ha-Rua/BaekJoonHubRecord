200bd390b5201b2eb4f00553ca87bd9db74924d2
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2026-01-10 22:25:04 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+struct Point {
+    long long x, y;
+    bool operator<(const Point& other) const
+    {
+        if (x != other.x) return x < other.x;
+        else return y < other.y;
+    }
+    bool operator<=(const Point& other) const
+    {
+        if (x != other.x) return x < other.x;
+        else return y <= other.y;        
+    }
+};
+int ccw(Point p1, Point p2, Point p3) {
+    long long res = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+    if (res > 0) return 1;
+    if (res < 0) return -1;
+    return 0;
+}
+
+bool isIntersect(pair<Point, Point> l1, pair<Point, Point> l2)
+{
+    Point p1 = l1.first;
+    Point p2 = l1.second;
+    Point p3 = l2.first;
+    Point p4 = l2.second;
+    
+    int p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4);
+    int p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2);
+
+    if (p1p2 == 0 && p3p4 == 0)
+    {
+        if (p2 < p1) swap(p2, p1);
+        if (p4 < p3) swap(p3, p4);
+        return p3 <= p2 && p1 <= p4;    
+    }
+    return p1p2 <= 0 && p3p4 <= 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    Point p1, p2, p3, p4;
+    cin >> p1.x >> p1.y >> p2.x >> p2.y;
+    cin >> p3.x >> p3.y >> p4.x >> p4.y;
+
+    cout << (isIntersect({p1, p2}, {p3, p4}) ? 1 : 0);
+
+    return 0;
+}
