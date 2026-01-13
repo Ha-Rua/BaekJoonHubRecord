c4c538d31e0b8f9e47317a1610913d111e4979c8
Ha-Rua <162019457+Ha-Rua@users.noreply.github.com>
2026-01-13 12:00:07 +0900

    [Gold V] Title: 두 용액, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin >> n;
+    vector<int> v;
+    v.reserve(n);
+    for (int i = 0; i < n; i++)
+    {
+        int a; cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+
+    int start = 0;
+    int end = n - 1;
+    int ans1 = v[0];
+    int ans2 = v[n - 1];
+    int minDiff = 2000000001;
+
+    while (start < end)
+    {
+        int cur = v[start] + v[end];
+        if (abs(cur) < minDiff)
+        {
+            minDiff = abs(cur);
+            ans1 = v[start];
+            ans2 = v[end];
+        }
+        if (cur == 0) break;
+        if (cur < 0) start++;
+        if (cur > 0) end--;
+    }
+    cout << ans1 << ' ' << ans2;
+    return 0;
+}
